4e60aaf55f3014eac5d3b83cf45d1b60cf312931
Satish Kumar <sk534@hotmail.com>
2022-02-03 18:10:16 +0530

    if elif else conditionals

---
+++ Day2/BMI_calc.py
@@ -0,0 +1,17 @@
+ht = float(input("Enter ur height in m: \n"))
+wt = float(input("Enter ur weight in kg: \n"))
+
+# bmi = wt / ht ** 2
+# print(bmi) #TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+
+bmi = round(wt / ht ** 2)
+if bmi < 18.5:
+  print(f"Your  bmi is {bmi}, underweight")
+elif bmi < 25:
+  print(f"Your  bmi is {bmi}, normalweight")
+elif bmi < 30:
+  print(f"Your  bmi is {bmi}, overweight")
+elif bmi < 35:
+  print(f"Your  bmi is {bmi}, obese")
+else:
+  print(f"Your  bmi is {bmi}, clinically obese")
