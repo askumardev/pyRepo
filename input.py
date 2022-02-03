e060e4e676bee970e0c9d3df029760626460009a
Satish Kumar <sk534@hotmail.com>
2022-02-03 12:24:25 +0530

    number swap added

--- input.py
+++ input.py
@@ -1,9 +1,15 @@
 # works in python3
 print("hello " + input("What is your name?"))
-print("*************")
 
+print("*************")
 print(len(input("What is your name...?")))
 print(len("satish"))
+
 print("*************")
 name = input("What is your name?")
 print(name)
+
+print("*************")
+name = input("What is your name?")
+length = len(name)
+print(length)
---
+++ number_swap.py
@@ -0,0 +1,12 @@
+a = input("a:")
+b = input("b:")
+# swap logic
+tmp = a
+a = b
+b = tmp
+
+print("*************After swapping")
+print("a="+ a)
+print("b="+ b)
+
+
