1f49f841a88391f84402fc05f753fffd7810d184
Satish Kumar <sk534@hotmail.com>
2022-02-03 18:57:31 +0530

    logical operators largest py progs added

---
+++ Day2/logical_op.py
@@ -0,0 +1,8 @@
+x = True
+y = False
+
+print('x and y is',x and y) #True and False => False
+
+print('x or y is',x or y) #True or False => True
+
+print('not x is',not x) #not True => False
---
+++ Day2/str_exs.py
@@ -0,0 +1,5 @@
+str1 = "Satish".lower()
+print(str1)
+
+str2 = str1.count("s")
+print(str2)
