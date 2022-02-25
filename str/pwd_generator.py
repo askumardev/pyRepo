a2bd275fe9b08e53ac288b18a3eb58625e36d20c
askumardev <askumar.dev@outlook.com>
2022-02-25 12:57:06 +0530

    update here

--- str/pwd_generator.py
+++ str/pwd_generator.py
@@ -1,15 +1,14 @@
 import random
 import string
 
-def get_random_string(length):
+def password(length):
   # choose from all lowercase letter
   letters = string.ascii_letters + string.digits + string.punctuation
   print(letters)
   result_str = ''.join(random.choice(letters) for i in range(length))
   print("Random string of length", length, "is:", result_str)
 
-get_random_string(8)
-
+password(8)
 
 # # get random password pf length 8 with letters, digits, and symbols
 # characters = string.ascii_letters + string.digits + string.punctuation
