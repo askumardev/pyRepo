44ff090d6ef9c00c338eed79b637f0f259f1d31b
askumardev <askumar.dev@outlook.com>
2022-07-08 15:57:51 +0530

    number conversions added

---
+++ py_concepts/number_conversion.py
@@ -0,0 +1,8 @@
+print(bin(25)) #0b11001 --> decimal to binary
+print(0b11001) #25 --> binary to decimal
+
+print(oct(25)) #0o31 -->decimal to octal
+print(0o30)  #24  --> octal to decimal
+
+print(hex(25)) #0x19 -->decimal to hedadecimal
+print(0x16)  #22 hexadecimal to decimal
