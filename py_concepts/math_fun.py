6884165f4f4d3d8a181640b8e6e38cea31b3442d
askumardev <askumar.dev@outlook.com>
2022-07-08 19:03:14 +0530

    math fun added

---
+++ py_concepts/math_fun.py
@@ -0,0 +1,10 @@
+import math
+#from math import sqrt, pow (can only sqrt and pow functions)
+
+print(math.sqrt(25)) #5.0
+
+print(math.floor(2.9)) #2
+print(math.ceil(2.2)) #3
+print(math.pow(3,2)) #9.0
+print(math.pi) #3.141592653589793
+print(math.e) #2.718281828459045
