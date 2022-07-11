7b974be8fc8357dc6ad5e8e9527ef7fdd8c5bc62
askumardev <askumar.dev@outlook.com>
2022-07-11 13:51:09 +0530

    While loop added

---
+++ pyConcepts/while_check.py
@@ -0,0 +1,33 @@
+i = 1
+while i<=5:
+  print("i::",+i)
+  i=i+1
+print("*******")
+y = 5
+while y>=1:
+  print("y::",+y)
+  y=y-1
+print("*****")
+
+'''Example to illustrate
+the use of else statement
+with the while loop'''
+
+counter = 0
+
+while counter < 3:
+  print("Inside loop")
+  counter = counter + 1
+else:
+  print("Inside else")
+
+print("******")
+i=1
+while i<=5:	
+  print("ASK ",end="")
+  j=1
+  while j<=4:
+    print("Rocks",end="")
+    j = j+1
+  i = i+1
+  print()
