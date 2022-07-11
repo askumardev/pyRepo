6361c192b63bfe58bfbb493c37c693eb7d45590b
askumardev <askumar.dev@outlook.com>
2022-07-11 13:58:58 +0530

    for loop added

---
+++ pyConcepts/for_loop.py
@@ -0,0 +1,18 @@
+x = [1,2,3]
+str = "satish"
+print(x)
+
+for i in x:
+  print(i)
+
+for i in str:
+  print(i)
+
+for i in range(10):
+  print(i)
+
+for i in range(11,21,2):
+  print(i)
+
+for i in range(20,10,-2):
+  print(i)
