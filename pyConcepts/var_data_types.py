6e9ef0535ebf18ed182dd5e60be1d6b730756bd0
askumardev <askumar.dev@outlook.com>
2022-07-12 16:24:38 +0530

    var datatypes update

--- pyConcepts/var_data_types.py
+++ pyConcepts/var_data_types.py
@@ -4,7 +4,10 @@
 #Tuple, Set, String, Range, Dictionary
 #----------------------------------------
 #Numeric - int,float,complex,bool
-#----------------------------------------
+#-----------------------------------------------------------------------------
+# Immutable data types --> int,long,float,complex,str,byte,stuple,frozenset
+# Mutable data types --> bytearray,list,set,dict
+#------------------------------------------------------------------------------
 
 #String
 str1 = "Hello, world..."
@@ -14,7 +17,7 @@ print(type(str1)) #<class 'str'>
 #Integer
 x = 123
 print(x) #123
-print(type(x)) #<class 'str'>
+print(type(x)) #<class 'int'>
 
 #Float
 pi = 3.14159
@@ -45,3 +48,9 @@ print(b) #5
 
 c = float(b)
 print(c) #5.0
+
+s = "123.456"
+#s1 = int(s) #ValueError: invalid literal for int() with base 10: '123.456'
+s2 = float(s)
+print(s2) #123.456
+print(int(s2)) #123
