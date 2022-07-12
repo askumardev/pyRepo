70f96f518f20140da70543e302d3c7a8af3a7dc9
askumardev <askumar.dev@outlook.com>
2022-07-12 12:42:53 +0530

    variables.py updated

--- pyConcepts/variables.py
+++ pyConcepts/variables.py
@@ -1,7 +1,12 @@
 x = 2
-y = 3
-z = x + y
-#a = _ + x
-# _ works in console
-print(z)
-#print(a)
+PI = 3.14
+str = "satish"
+
+q = True
+y = False
+
+print(x) #2 (integer)
+print(PI) #3.14 (Float)
+print(str) #satish (string)
+print(q) #True (boolean)
+print(y) #False (boolean)
