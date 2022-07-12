c449eb04a63bc0e828101a9d901740760e1524c4
askumardev <askumar.dev@outlook.com>
2022-07-12 12:49:27 +0530

    variables update

--- pyConcepts/variables.py
+++ pyConcepts/variables.py
@@ -1,3 +1,5 @@
+# variable names must start with letter or underscore
+#variable can have multiple underscores
 x = 2
 PI = 3.14
 str = "satish"
@@ -10,3 +12,10 @@ print(PI) #3.14 (Float)
 print(str) #satish (string)
 print(q) #True (boolean)
 print(y) #False (boolean)
+
+a = True  #Valid
+_b = False #Valid
+a_b_c_d = 8 #valid
+
+#9x = False #SyntaxError: invalid syntax
+#$y = False #SyntaxError: invalid syntax
