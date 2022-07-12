35f70b8ef49dfe1449a13c88a46bcf3787d8ce6f
askumardev <askumar.dev@outlook.com>
2022-07-12 12:55:45 +0530

    variables.py updated1

--- pyConcepts/var_data_types.py
+++ pyConcepts/var_data_types.py
@@ -8,35 +8,18 @@
 
 #String
 str1 = "Hello, world..."
-print(str1[3])
-print(type(str1))
+print(str1) #Hello, world...
+print(type(str1)) #<class 'str'>
 
 #Integer
 x = 123
 print(x) #123
-print(x + 7) #130
 print(type(x)) #<class 'str'>
-print(x) #123
-x = x + 17
-print(x) #140
-#print(len(x)) #TypeError: object of type 'int' has no len()
-str_x = str(x)
-len_x = len(str_x)
-print(len_x) #3
-float_x = float(x)
-print(float_x) #140.0
-print("***************")
-y = 1234567890000000000
-print(y)
-print(type(y)) #<class 'int'>
 
 #Float
 pi = 3.14159
-print(pi)
+print(pi) #3.14159
 print(type(pi)) #<class 'float'>
-int_pi = int(pi)
-print(int_pi)
-#print(len(pi)) #TypeError: object of type 'float' has no len()
 
 #complex
 num = 6 + 9j
@@ -44,12 +27,16 @@ print(type(num)) #<class 'complex'>
 print(num) #(6+9j)
 
 #Boolean
-bool = True
-print(bool)
-print(type(bool)) #<class 'bool'>
-bool = False
-print(bool)
-print(type(bool)) #<class 'bool'>
+boolx = True
+print(boolx) #True
+print(type(boolx)) #<class 'bool'>
+booly = False
+print(booly) #False
+print(type(booly)) #<class 'bool'>
+
+a = None
+print(a) #None
+print(type(a)) #<class 'NoneType'>
 
 #type conversions
 a = 5.6
