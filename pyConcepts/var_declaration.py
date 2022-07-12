08c1a614db3bcec277b1074b49a50ca13ed0e2be
askumardev <askumar.dev@outlook.com>
2022-07-12 13:02:47 +0530

    var_declaration added

---
+++ pyConcepts/var_declaration.py
@@ -0,0 +1,17 @@
+a = b = c = 2
+print(a,b,c) #2 2 2
+
+a,b,c = 1,2,3
+print(a,b,c) #1 2 3
+
+a,b,_ = 4,5,6
+print(a,b,_) #4 5 6
+
+# a,b,_ = 4,5,6,7
+# print(a,b,_) #4 5 6 #ValueError: too many values to unpack (expected 3)
+
+#a,b,c = 1,2
+#print(a,b,c) #ValueError: not enough values to unpack (expected 3, got 2)
+
+#a,b = 1,2,3
+#print(a,b,c) #ValueError: too many values to unpack (expected 2)
