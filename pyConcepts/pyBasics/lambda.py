b908577d73276f1687e0fad5270dd048962279e4
askumardev <askumar.dev@outlook.com>
2022-07-21 16:25:55 +0530

    lambda updated

--- pyConcepts/pyBasics/lambda.py
+++ pyConcepts/pyBasics/lambda.py
@@ -3,6 +3,6 @@
 # A lambda function can take any number of arguments, but can only have one expression.
 
 x = lambda a : a + 10
-print(x(5))
+print(x(5)) #15
 
 # python3 pyConcepts/pyBasics/lambda.py 
