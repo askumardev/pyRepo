bf9b4465d35cb8c57ead73e730ca40132dc947b4
askumardev <askumar.dev@outlook.com>
2022-07-21 16:11:06 +0530

    break continue added

---
+++ pyConcepts/pyBasics/break_continue.py
@@ -0,0 +1,12 @@
+for i in range(5):
+  if i==3:
+    break
+  print("hello ",i)  
+
+for i in range(1,20):
+  if i%3==0 or i%5==0:
+    continue
+  print(i)
+print("Bye")
+
+#python3 pyConcepts/pyBasics/break_continue.py 
