7a046d26070103d0d14f38b8ae5085222d3d0d48
askumardev <askumar.dev@outlook.com>
2022-09-07 21:41:58 +0530

    calender added

--- pyConcepts/date_time/time.py
+++ pyConcepts/date_time/time.py
@@ -1,4 +1,5 @@
 import time;      # This is required to include time module.
+import calendar;
 
 ticks = time.time()
 print ("Number of ticks since 12:00am, January 1, 1970:", ticks)
@@ -10,5 +11,9 @@ print ("Local current time :", localtime)
 
 localtime = time.asctime( time.localtime(time.time()) )
 print ("Local current time :", localtime)
+
+cal = calendar.month(2022, 9)
+print ("Here is the calendar:")
+print (cal)
 
 #python3 pyConcepts/date_time/time.py
