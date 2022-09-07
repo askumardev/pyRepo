cc6549eace11ccf90f12e0167dc24b5212695544
askumardev <askumar.dev@outlook.com>
2022-09-07 21:36:45 +0530

    local time check

--- pyConcepts/date_time/time.py
+++ pyConcepts/date_time/time.py
@@ -3,4 +3,6 @@ import time;      # This is required to include time module.
 ticks = time.time()
 print ("Number of ticks since 12:00am, January 1, 1970:", ticks)
 
+print (time.localtime());
+
 #python3 pyConcepts/date_time/time.py
