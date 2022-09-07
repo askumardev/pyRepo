1a596559b0fe56c364b931bccd14c0bcab9383d8
askumardev <askumar.dev@outlook.com>
2022-09-07 21:38:32 +0530

    localtime update

--- pyConcepts/date_time/time.py
+++ pyConcepts/date_time/time.py
@@ -5,4 +5,7 @@ print ("Number of ticks since 12:00am, January 1, 1970:", ticks)
 
 print (time.localtime());
 
+localtime = time.localtime(time.time())
+print ("Local current time :", localtime)
+
 #python3 pyConcepts/date_time/time.py
