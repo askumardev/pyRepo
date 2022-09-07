2b7150969b29188e031f77bb2b4e62ed8fa5bb84
askumardev <askumar.dev@outlook.com>
2022-09-07 21:48:03 +0530

    multi params added

--- pyConcepts/pyBasics/fun_params.py
+++ pyConcepts/pyBasics/fun_params.py
@@ -1,6 +1,19 @@
 def greet(str):
 	print(str)
 
+def printinfo( arg1, *vartuple ):
+	"This prints a variable passed arguments"
+	print ("Output is: ")
+	print (arg1)
+
+	for var in vartuple:
+		print ("in loop", +var)
+	return
+
 greet("Hello")
+
+# Now you can call printinfo function
+printinfo( 10 )
+printinfo( 70, 60, 50 )
 
 # python3 pyConcepts/pyBasics/fun_params.py
