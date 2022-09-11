cbc5378b47e18d651683f76fd5244006736fd1d2
askumardev <askumar.dev@outlook.com>
2022-09-11 20:54:28 +0530

    sample 1.py update

--- pyConcepts/sample/1.py
+++ pyConcepts/sample/1.py
@@ -1,6 +1,6 @@
 print("sample printing of " + str(50) + " here")
 
-print(f"Converting days to minutes {50 * 24 * 60} here")
+print(f"Converting 50 days to minutes are :: {50 * 24 * 60} ")
 print(3)
 
 #python3 pyConcepts/sample/1.py
