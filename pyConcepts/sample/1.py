6b0f68946fa0f966adfa63e84c738e507d66a19b
askumardev <askumar.dev@outlook.com>
2022-09-11 21:10:07 +0530

    author added

--- pyConcepts/sample/1.py
+++ pyConcepts/sample/1.py
@@ -7,4 +7,10 @@ print(f"Converting 50 days to seconds are :: {50 * 24 * 60 * 60} ")
 print(f"Converting 100 days to seconds are :: {100 * toUnits} by {author} ")
 print(3)
 
+
+def print_author():
+  print(f"Coding by {author} ")
+
+print_author()
+
 #python3 pyConcepts/sample/1.py
