c593db4247986313d08fef1732b7f21fd09b414f
askumardev <askumar.dev@outlook.com>
2022-09-11 21:05:13 +0530

    interpolation added

--- pyConcepts/sample/1.py
+++ pyConcepts/sample/1.py
@@ -1,9 +1,10 @@
 toUnits = 24 * 60 * 60
+author = "satish"
 
 print("sample printing of " + str(50) + " here")
 print(f"Converting 50 days to minutes are :: {50 * 24 * 60} ")
 print(f"Converting 50 days to seconds are :: {50 * 24 * 60 * 60} ")
-print(f"Converting 100 days to seconds are :: {100 * toUnits} ")
+print(f"Converting 100 days to seconds are :: {100 * toUnits} by {author} ")
 print(3)
 
 #python3 pyConcepts/sample/1.py
