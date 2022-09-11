2b5cd36cc54deb94bc396895fc0e5c9d499a3667
askumardev <askumar.dev@outlook.com>
2022-09-11 20:55:27 +0530

    days to seconds added

--- pyConcepts/sample/1.py
+++ pyConcepts/sample/1.py
@@ -1,6 +1,7 @@
 print("sample printing of " + str(50) + " here")
 
 print(f"Converting 50 days to minutes are :: {50 * 24 * 60} ")
+print(f"Converting 50 days to seconds are :: {50 * 24 * 60 * 60} ")
 print(3)
 
 #python3 pyConcepts/sample/1.py
