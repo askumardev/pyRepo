2c24bdc6cbbb965cbd674ed9265bf67e73f2c1b1
askumardev <askumar.dev@outlook.com>
2022-09-11 21:03:35 +0530

    update

--- pyConcepts/sample/1.py
+++ pyConcepts/sample/1.py
@@ -1,6 +1,6 @@
-print("sample printing of " + str(50) + " here")
 toUnits = 24 * 60 * 60
 
+print("sample printing of " + str(50) + " here")
 print(f"Converting 50 days to minutes are :: {50 * 24 * 60} ")
 print(f"Converting 50 days to seconds are :: {50 * 24 * 60 * 60} ")
 print(f"Converting 100 days to seconds are :: {100 * toUnits} ")
