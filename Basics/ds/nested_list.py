27bf8a3cbca688a7879e3c29f440ab398eed4f5c
askumardev <askumar.dev@outlook.com>
2022-09-21 13:16:34 +0530

    basics update

---
+++ Basics/ds/nested_list.py
@@ -0,0 +1,12 @@
+L = ['a', 'b', ['cc', 'dd', ['eee', 'fff']], 'g', 'h']
+
+print(L[2])
+# Prints ['cc', 'dd', ['eee', 'fff']]
+
+print(L[2][2])
+# Prints ['eee', 'fff']
+
+print(L[2][2][0])
+# Prints eee
+
+# python3 Basics/ds/nested_list.py
