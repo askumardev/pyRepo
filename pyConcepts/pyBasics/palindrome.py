2bb09df160ed564e8f4ead64261f7baab187ab08
askumardev <askumar.dev@outlook.com>
2022-09-21 12:24:53 +0530

    palindrome update

--- pyConcepts/pyBasics/palindrome.py
+++ pyConcepts/pyBasics/palindrome.py
@@ -1,8 +1,17 @@
-def pal(num):
-    x1 = num[::-1]
-    if x1 == x:
-      print('palindrome')
-    else:
-      print('not a palindrome')
- 
-print(pal('edureka'))
+# Program to check if a string is palindrome or not
+
+my_str = 'aIbohPhoBiA'
+
+# make it suitable for caseless comparison
+my_str = my_str.casefold()
+
+# reverse the string
+rev_str = reversed(my_str)
+
+# check if the string is equal to its reverse
+if list(my_str) == list(rev_str):
+  print("The string is a palindrome.")
+else:
+  print("The string is not a palindrome.")
+
+# python3 pyConcepts/pyBasics/palindrome.py
