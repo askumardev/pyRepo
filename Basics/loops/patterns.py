3217f7ea3d47a330eb6416fd6e5039bba7c2d151
askumardev <askumar.dev@outlook.com>
2022-09-24 20:21:15 +0530

    patterns added

---
+++ Basics/loops/patterns.py
@@ -0,0 +1,22 @@
+# python3 Basics/loops/patterns.py
+
+for i in range(4):
+  for j in range(4):
+    print("# ",end="")
+  
+  print()
+print("###############################")
+
+for i in range(4):
+  for j in range(i+1):
+    print("# ",end="")
+  
+  print()
+
+print("###############################")
+
+for i in range(4):
+  for j in range(4-i):
+    print("# ",end="")
+  
+  print()
