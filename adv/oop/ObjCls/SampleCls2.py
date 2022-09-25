059418eaedd2bc287a2b27d4651ba13363169d8b
askumardev <askumar.dev@outlook.com>
2022-09-25 14:55:24 +0530

    samplecls2 update

---
+++ adv/oop/ObjCls/SampleCls2.py
@@ -0,0 +1,16 @@
+# python3 adv/oop/ObjCls/SampleCls2.py
+
+class Computer:
+
+  def __init__(self,cpu,ram):
+    self.cpu = cpu
+    self.ram = ram
+
+  def config(self):
+    print("Hello Computer",self.cpu,self.ram)
+
+com1 = Computer("i5",16)
+com2 = Computer("intel",8)
+
+com1.config() #
+com2.config()
