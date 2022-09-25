c43494e59d9b3e6a791b5242abcb144318860a52
askumardev <askumar.dev@outlook.com>
2022-09-25 14:05:48 +0530

    calc demo module

---
+++ adv/oop/modDemo/calc_demo.py
@@ -0,0 +1,17 @@
+# python3 adv/oop/modDemo/calc_demo.py
+
+# import Calculator
+
+# a = 9
+# b = 8
+
+# c = Calculator.add(a,b)
+# print(c)
+
+from Calculator import *
+
+a = 9
+b = 8
+
+c = add(a,b)
+print(c)
