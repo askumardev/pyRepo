441c71b9b8d125a452f6db2101fee0a0d6dfee2e
askumardev <askumar.dev@outlook.com>
2022-09-25 14:48:39 +0530

    cls obj added

---
+++ adv/oop/ObjCls/SampleCls.py
@@ -0,0 +1,13 @@
+# python3 adv/oop/ObjCls/SampleCls.py
+
+class Computer:
+
+  def config(self):
+    print("Hello Computer")
+
+com = Computer()
+print(type(com))
+
+com.config()
+# Computer.config() #TypeError: config() missing 1 required positional argument: 'self'
+Computer.config(com)
