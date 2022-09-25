ff50472b9c30090144eddd2e5fbf78990d673dc9
askumardev <askumar.dev@outlook.com>
2022-09-25 11:14:45 +0530

    first fun update

--- Basics/first_function.py
+++ Basics/first_function.py
@@ -1,7 +1,26 @@
+# python3 Basics/first_function.py
+
 def fun():
 	print("*****first function*****")
+
+def add(a,b):
+	c = a + b
+	#print(c)
+	return c
+
+def add_sub(a,b):
+	c = a + b
+	d = a - b
+	#print(c)
+	return c,d
 
 fun() #*****first function*****
 print(fun()) #*****first function***** && None
 
-# python3 Basics/first_function.py
+res = add(5,4)
+print(res)
+
+res1,res2 = add_sub(15,4)
+print(res1,res2)
+
+#A function can execute instructions and return a value
