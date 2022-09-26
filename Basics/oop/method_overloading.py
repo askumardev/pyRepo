b5bf2697078b2f0345439a37a0a1e2063e295fa1
askumardev <askumar.dev@outlook.com>
2022-09-26 12:35:54 +0530

    method overloading

---
+++ Basics/oop/method_overloading.py
@@ -0,0 +1,16 @@
+# python3 Basics/oop/method_overloading.py
+
+class Person:
+
+  def Hello(self, name=None):
+    if name is not None:
+      print('Hello ' + name)
+    else:
+      print('Hello ')
+
+# Create instance
+obj = Person()
+# Call the method
+obj.Hello()
+# Call the method with a parameter
+obj.Hello('ABC')
