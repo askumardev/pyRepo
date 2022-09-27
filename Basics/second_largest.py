aedd2e9db7b7bad1b7abcc6cb4105e2ec204bb8f
askumardev <askumar.dev@outlook.com>
2022-09-27 19:21:00 +0530

    second largest added

---
+++ Basics/second_largest.py
@@ -0,0 +1,17 @@
+# python3 Basics/second_largest.py
+
+list_val = []  
+  
+# user provides the number of elements to be added in the list  
+num_list = int(input("Enter number of elements in list: "))  
+  
+  
+for i in range(1, num_list + 1):  
+  element = int(input("Enter the elements: "))  
+  list_val.append(element)  
+  
+# sort the list  
+list_val.sort()  
+      
+# print second largest element  
+print("Second largest element is:", list_val[-2])  
