e8724bcfddd65959a15787fcd067a4e07943503f
askumardev <askumar.dev@outlook.com>
2022-10-02 20:32:18 +0530

    str to date added

---
+++ adv/date_time/str_to_date.py
@@ -0,0 +1,10 @@
+# python3 adv/date_time/str_to_date.py
+
+from datetime import datetime
+
+my_date_string = "Mar 11 2011 11:31AM"
+
+datetime_object = datetime.strptime(my_date_string, '%b %d %Y %I:%M%p')
+
+print(type(datetime_object))
+print(datetime_object)
