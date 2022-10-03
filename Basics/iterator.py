7af877fb3fbc772ce5c7a7813625cc5d9c87fe2e
askumardev <askumar.dev@outlook.com>
2022-10-03 20:36:57 +0530

    iterator added

---
+++ Basics/iterator.py
@@ -0,0 +1,12 @@
+# python3 Basics/iterator.py
+
+arr = [2,3,4,5,6]
+
+it = iter(arr)
+print(it.__next__())
+print(it.__next__())
+print("*****")
+print(next(it))
+print("#####")
+for i in arr:
+  print(i)
