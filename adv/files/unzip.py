89422d1d13a8f2a65e0718704d2a55be54e3ac5a
askumardev <askumar.dev@outlook.com>
2022-10-10 14:06:06 +0530

    unzip

---
+++ adv/files/unzip.py
@@ -0,0 +1,12 @@
+# python3 adv/files/unzip.py
+
+coordinate = ['x', 'y', 'z']
+value = [3, 4, 5]
+
+result = zip(coordinate, value)
+result_list = list(result)
+print(result_list)
+
+c, v =  zip(*result_list)
+print('c =', c)
+print('v =', v)
