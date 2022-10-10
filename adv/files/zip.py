4436d96e5d327f221722bee15a7855bb98377139
askumardev <askumar.dev@outlook.com>
2022-10-10 14:03:04 +0530

    zip func

---
+++ adv/files/zip.py
@@ -0,0 +1,44 @@
+languages = ['Java', 'Python', 'JavaScript']
+versions = [14, 3, 6]
+
+result = zip(languages, versions)
+print(list(result))
+
+# Output: [('Java', 14), ('Python', 3), ('JavaScript', 6)]
+print("**************************************************")
+
+number_list = [1, 2, 3]
+str_list = ['one', 'two', 'three']
+
+# No iterables are passed
+result = zip()
+
+# Converting iterator to list
+result_list = list(result)
+print(result_list)
+
+# Two iterables are passed
+result = zip(number_list, str_list)
+
+# Converting iterator to set
+result_set = set(result)
+print(result_set)
+print("**************************************************")
+
+numbersList = [1, 2, 3]
+str_list = ['one', 'two']
+numbers_tuple = ('ONE', 'TWO', 'THREE', 'FOUR')
+
+# Notice, the size of numbersList and numbers_tuple is different
+result = zip(numbersList, numbers_tuple)
+
+# Converting to set
+result_set = set(result)
+print(result_set)
+
+result = zip(numbersList, str_list, numbers_tuple)
+
+# Converting to set
+result_set = set(result)
+print(result_set)
+
