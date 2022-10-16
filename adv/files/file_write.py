e07f7c0088a58ad5882d19356df15c6e29a6820c
askumardev <askumar.dev@outlook.com>
2022-10-16 13:51:30 +0530

    write file changes

--- adv/files/file_write.py
+++ adv/files/file_write.py
@@ -4,4 +4,10 @@ foo.write( "Python is a great language.\nYes...\n")
 # Close opend file
 foo.close()
 
+foo = open("foo.txt", "a")
+foo.write( "adding new text")
+
+# Close opend file
+foo.close()
+
 # python3 adv/files/file_write.py
