eb13a1ea22bc2ce8480f71567a0a31326fc94e8c
askumardev <askumar.dev@outlook.com>
2022-10-16 15:34:10 +0530

    socket update

--- adv/network/socket_prog_client.py
+++ adv/network/socket_prog_client.py
@@ -1,25 +1,26 @@
+# python3 adv/network/socket_prog_client.py
+
 import socket
 
-
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
+  host = socket.gethostname()  # as both code is running on same pc
+  port = 5000  # socket server port number
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+  client_socket = socket.socket()  # instantiate
+  client_socket.connect((host, port))  # connect to the server
 
-    message = input(" -> ")  # take input
+  message = input(" -> ")  # take input
 
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
+  while message.lower().strip() != 'bye':
+    client_socket.send(message.encode())  # send message
+    data = client_socket.recv(1024).decode()  # receive response
 
-        print('Received from server: ' + data)  # show in terminal
+    print('Received from server: ' + data)  # show in terminal
 
-        message = input(" -> ")  # again take input
+    message = input(" -> ")  # again take input
 
-    client_socket.close()  # close the connection
+  client_socket.close()  # close the connection
 
 
 if __name__ == '__main__':
-    client_program()
+  client_program()
