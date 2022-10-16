8d98a0789cbd90543b3a34f0ffedb6cea7dc6483
askumardev <askumar.dev@outlook.com>
2022-10-16 14:06:50 +0530

    abt python update

--- pyNotes/about_python.py
+++ pyNotes/about_python.py
@@ -1,5 +1,6 @@
 # Python is a widely used high-level programming for general purpose progamming 
 # Created by Guido van Rossum in 1991.
+#Python is both compiled and interpreted language
 # Python features 
 # * Dynamic type system
 # * Automatic memory management
