3da3091ec2e667f1f68f4442715040d01407bd80
askumardev <askumar.dev@outlook.com>
2022-10-16 14:21:37 +0530

    bsearch update

--- Basics/ds/binary_search.py
+++ Basics/ds/binary_search.py
@@ -25,12 +25,12 @@ def binary_search(list1, n):
   return -1
 
 # Initial list1  
-list1 = [12, 24, 32, 39, 45, 50, 54]  
-n = 45  
-  
+list1 = [12, 24, 32, 39, 45, 50, 54]
+n = 45
+
 # Function call   
 result = binary_search(list1, n)
-  
+
 if result != -1:
   print("Element is present at index", str(result))
 else:
