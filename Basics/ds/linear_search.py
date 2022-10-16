2622d6300a52dc11dc5fc75dd751d806810088ee
askumardev <askumar.dev@outlook.com>
2022-10-16 14:16:36 +0530

    linear search update

--- Basics/ds/linear_search.py
+++ Basics/ds/linear_search.py
@@ -1,14 +1,13 @@
 # python3 Basics/ds/linear_search.py
 
 def linear_Search(list1, n, key):
-  
-  # Searching list1 sequentially 
+
+  # Searching list1 sequentially
   for i in range(0, n):  
     if (list1[i] == key):
       return i
   return -1
-  
-  
+
 list1 = [1 ,3, 5, 4, 7, 9]
 key = 7
 
