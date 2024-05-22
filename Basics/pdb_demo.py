9227adf977139a08916d3c791743566b1a5c63e9
askumardev <askumar.dev@outlook.com>
2024-05-22 13:37:21 +0530

    pdb demo update

--- Basics/pdb_demo.py
+++ Basics/pdb_demo.py
@@ -5,10 +5,13 @@ import pdb
 def addition(a, b):
   answer = a + b
   return answer
- 
- 
+  
 pdb.set_trace()
 x = int(input("Enter first number : "))
 y = int(input("Enter second number : "))
 sum = addition(x, y)
 print("Sum of x and y is :",sum)
+
+#use the following cmds
+# break <line num> --to set break point
+# continue
