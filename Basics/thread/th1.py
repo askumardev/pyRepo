26aed4a545842116a72660abc64a048f9d8905e7
askumardev <askumar.dev@outlook.com>
2024-05-23 18:30:27 +0530

    basic threads

---
+++ Basics/thread/th1.py
@@ -0,0 +1,16 @@
+import threading
+
+def print_numbers():
+  for i in range(10):
+      print(i)
+
+# Create a Thread object
+thread = threading.Thread(target=print_numbers)
+
+# Start the thread
+thread.start()
+
+# Wait for the thread to complete
+thread.join()
+
+# python3 Basics/thread/th1.py
