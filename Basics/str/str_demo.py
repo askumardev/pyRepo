b26f39db4e4cdef7886dcdf98f4338c2dc83f87c
askumardev <askumar.dev@outlook.com>
2024-06-10 20:10:49 -0700

    str demo update2

--- Basics/str/str_demo.py
+++ Basics/str/str_demo.py
@@ -14,15 +14,22 @@ print(str[::2]) #aceg
 
 #Basic methods
 print(str.upper())
+str1 = "Hello World"
+print(str1.split()) #['Hello', 'World']
+print(str1.split('e')) #['H', 'llo World']
 
 # Use the dir() function to get all attributes and methods of the string object
 string_methods = dir(str)
 # Display the methods
-print(string_methods)
+# print(string_methods)
 # ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 
 # 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
 
 # Display help for the count method
 print(len(string_methods)) #81
+
+#Print Formatting
+x = "Insert some thing here: {}".format("INSERT ME!")
+print(x)
 
 # python3 adv/str/str_demo.py
