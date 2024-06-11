fcb2875274bd492847167b054690a97d7e8c59c4
askumardev <askumar.dev@outlook.com>
2024-06-10 19:42:15 -0700

    str demo update

--- Basics/str/str_demo.py
+++ Basics/str/str_demo.py
@@ -1,10 +1,14 @@
-print("satish's laptop")
-#satish's laptop
+print("satish's laptop") #satish's laptop
 
-print('satish "kumar"')
-#satish "kumar"
+print('satish "kumar"') #satish "kumar"
 
-print('satish\'s "laptop"')
-#satish's "laptop"
+print('satish\'s "laptop"') #satish's "laptop"
+
+str = "abcdefgh"
+# Slicing
+print(str[3:]) #defgh
+print(str[:5]) #abcde
+print(str[2:5]) #cde
+
 
 # python3 adv/str/str_demo.py
