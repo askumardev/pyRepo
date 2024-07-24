97c5c03cabe6dd2f675958422f4d95adf0998735
askumardev <askumar.dev@outlook.com>
2024-07-24 19:48:34 +0530

    arth by input

---
+++ Basics/arithmatic.py
@@ -0,0 +1,35 @@
+# python3 Basics/arithmatic.py
+
+
+def add(a,b):
+	c = a + b
+	#print(c)
+	return c
+
+def sub(a,b):
+	c = a - b
+	#print(c)
+	return c
+
+def mul(a,b):
+	c = a * b
+	#print(c)
+	return c
+
+def div(a,b):
+	c = a / b
+	#print(c)
+	return c
+
+res = add(5,4)
+print(res)
+
+res1 = sub(15,4)
+print(res1)
+
+res1 = mul(5,4)
+print(res1)
+
+res1 = div(5,4)
+print(res1)
+#A function can execute instructions and return a value
---
+++ adv/math/arth_by_input.py
@@ -0,0 +1,12 @@
+#python3 adv/math/arth_by_input.py
+
+a = int(input("Enter value a :"))
+b = int(input("Enter value b :"))
+print(a,b)
+
+print("Addition = ", a + b) 
+print("Substraction = ",b - a) 
+print("Multiplication = ",a * b) 
+print("Division = ", b / a) 
+print("Integer Division = ", b // a) 
+print("Modulus Division = ", 10 % a) 
