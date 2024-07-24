4a49a5f8f05a0721f1499e388fe5cd1bab75d19c
askumardev <askumar.dev@outlook.com>
2024-07-24 19:17:23 +0530

    add_digits

---
+++ Basics/add_digits.py
@@ -0,0 +1,14 @@
+# python3 Basics/add_digits.py
+
+
+def add_digits(num):
+    if len(str(num)) < 2:
+        return num
+    arr = [int(x) for x in str(num)]
+    sum_digits = sum(arr)
+    return add_digits(sum_digits)
+
+print(add_digits(12))
+print(add_digits(123))
+print(add_digits(1234))
+print(add_digits(8))
