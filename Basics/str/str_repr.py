e645f80b0e7632916c3cf250a8a0327ba5272860
askumardev <askumar.dev@outlook.com>
2024-11-25 21:02:57 +0530

    str update

--- Basics/str/str_repr.py
+++ Basics/str/str_repr.py
@@ -3,4 +3,4 @@ today = datetime.datetime.now()
 print(str(today)) #2022-07-13 21:12:23.442845
 print(repr(today)) #datetime.datetime(2022, 7, 13, 21, 12, 23, 442845)
 
-# python3 adv/str/str_repr.py
+# python3 Basics/str/str_repr.py
