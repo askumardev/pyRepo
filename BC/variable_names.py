13915e1d51574a8c7ba13b5cf4e8591c66979993
askumardev <askumar.dev@outlook.com>
2024-12-06 12:50:19 -0800

    variable names py

---
+++ BC/variable_names.py
@@ -0,0 +1,13 @@
+# Rules for variable names
+# names can not start with a number
+#
+# names can not contain spaces, use _ intead
+#
+# names can not contain any of these symbols:
+#
+# :'",<>/?|\!@#%^&*~-+
+# it's considered best practice (PEP8) that names are lowercase with underscores
+#
+# avoid using Python built-in keywords like list and str
+#
+# avoid using the single characters l (lowercase letter el), O (uppercase letter oh) and I (uppercase letter eye) as they can be confused with 1 and 0
