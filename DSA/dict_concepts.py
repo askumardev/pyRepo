18c8836ce6bdd72705a10789d89e61eaf834c52a
askumardev <askumar.dev@outlook.com>
2024-12-21 20:51:06 +0530

    DSA dict_concepts update

--- DSA/dict_concepts.py
+++ DSA/dict_concepts.py
@@ -9,4 +9,4 @@ print(data['Python'][1]) #sublime
 print(data['Java']) #{'jse': 'netbeans', 'jee': 'eclipse'}
 print(data['Java']['jse']) #netbeans
 
-# python3 Basics/ds/dict_concepts.py
+# python3 DSA/dict_concepts.py
