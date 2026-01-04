e42589a3a6faa1bb21e8996c55bf9c10cc3aacd6
askumardev <askumar.dev@outlook.com>
2026-01-04 11:46:13 +0530

    dunder updates 1

--- Basics/dunder_methods.py
+++ Basics/dunder_methods.py
@@ -9,6 +9,7 @@ print(str1.__len__())
 # __str__() , __add__(), __sub__(), __mul__(), __repr__(), __init__()
 # __truediv__(), __eq__(), __lt__(), __gt__(), __gte__(), __ne__()
 # __getitem__(), __setitem__(), __delitem__(), __contains__(), __append__()
+# __enter__(), __exit__()
 
 def func():
     pass
