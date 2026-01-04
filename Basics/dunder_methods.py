ee39ebf6ae70782228bb5dde50fbab21ba841405
askumardev <askumar.dev@outlook.com>
2026-01-04 11:45:13 +0530

    dunder updates

--- Basics/dunder_methods.py
+++ Basics/dunder_methods.py
@@ -6,6 +6,9 @@ newStr = str1.__add__(str2)
 print(newStr)
 print(str1.__len__())
 
+# __str__() , __add__(), __sub__(), __mul__(), __repr__(), __init__()
+# __truediv__(), __eq__(), __lt__(), __gt__(), __gte__(), __ne__()
+# __getitem__(), __setitem__(), __delitem__(), __contains__(), __append__()
 
 def func():
     pass
