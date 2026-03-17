28b388ca15bdadc678fd1417d4e1100225bd49ea
askumardev <askumar.dev@outlook.com>
2026-03-17 11:36:16 +0530

    arth

--- Basics/math/arithimatic.py
+++ Basics/math/arithimatic.py
@@ -3,15 +3,11 @@
 a = 3
 b = 6
 print("Addition = ", a + b) #9
-
 print("Substraction = ",b - a) #3
-
 print("Multiplication = ",a * b) #18
-
 print("Division = ", b / a) #2.0
 print("Integer Division = ", b // a) #2
 print("Modulus Division = ", 10 % a) #1
-
 print("Exponent = ", 2**3) #8
 
 #PEMDASLR(Parenthesis, Exponents, Mul. Div, Add, Sub) Left to Right
