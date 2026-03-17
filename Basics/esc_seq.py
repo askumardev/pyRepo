b6b976a5ae0f0c7715334e9767956f34a8ec3459
askumardev <askumar.dev@outlook.com>
2026-03-17 11:22:56 +0530

    escape seq

---
+++ Basics/esc_seq.py
@@ -0,0 +1,13 @@
+# print("Hello
+#       world...") #SyntaxError: unterminated string literal (detected at line 1)
+
+print("Hello \nworld...")
+print("Hello \tworld...")
+print("Hello \rworld...")
+print("Hello \bworld...")
+print("Hello \fworld...")
+print("Hello \vworld...")
+print("Hello \\world...")
+print("Hello \'world...")
+print("Hello \"world...")
+print("Hello \nworld\\n123...")
