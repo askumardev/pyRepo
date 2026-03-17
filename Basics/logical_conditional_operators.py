50cec393152554b5f62ab6347e719aa47a4a8917
askumardev <askumar.dev@outlook.com>
2026-03-17 11:44:33 +0530

    logi cond ops 2

---
+++ Basics/logical_conditional_operators.py
@@ -0,0 +1,21 @@
+print("==============Logical operators=====================")
+x = True
+y = False
+a = 6
+b = 3
+print('x and y is',x and y) #True and False => False
+
+print('x or y is',x or y) #True or False => True
+
+print('not x is',not x) #not True => False
+
+print("==============Conditional operators=====================")
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+# python3 Basics/logical_conditional_operators.py
