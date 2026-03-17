572c09e275e62a142df788e6cf90cf77010c5529
askumardev <askumar.dev@outlook.com>
2026-03-17 18:13:02 +0530

    strong ped update

--- Basics/str/strong_pwd_generator.py
+++ Basics/str/strong_pwd_generator.py
@@ -3,11 +3,14 @@ import random, string
 def strong_password(length):
   # choose from all lowercase letter
   letters = string.ascii_letters + string.digits + string.punctuation
-  print(letters)
+  # print(letters)
   result_str = ''.join(random.choice(letters) for i in range(length))
   print("Random string of length", length, "is:", result_str)
 
 strong_password(8)
+print("-----------------------------")
+strong_password(10)
+
 
 # # get random password pf length 8 with letters, digits, and symbols
 # characters = string.ascii_letters + string.digits + string.punctuation
