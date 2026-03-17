d4a9804869cedc47adaa5cc26c761b741b879cac
askumardev <askumar.dev@outlook.com>
2026-03-17 18:09:05 +0530

    strong pwd

--- Basics/str/strong_pwd_generator.py
+++ Basics/str/strong_pwd_generator.py
@@ -1,18 +1,17 @@
-import random
-import string
+import random, string
 
-def password(length):
+def strong_password(length):
   # choose from all lowercase letter
   letters = string.ascii_letters + string.digits + string.punctuation
   print(letters)
   result_str = ''.join(random.choice(letters) for i in range(length))
   print("Random string of length", length, "is:", result_str)
 
-password(8)
+strong_password(8)
 
 # # get random password pf length 8 with letters, digits, and symbols
 # characters = string.ascii_letters + string.digits + string.punctuation
 # password = ''.join(random.choice(characters) for i in range(8))
 # print("Random password is:", password)
 
-# python3 Basics/str/pwd_generator.py
+# python3 Basics/str/strong_pwd_generator.py
