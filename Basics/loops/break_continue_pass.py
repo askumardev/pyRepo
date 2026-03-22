0f97a1a1f0ff341b195aa48967894d5433eb3107
askumardev <askumar.dev@outlook.com>
2026-03-22 07:14:21 +0530

    new break continue

--- Basics/loops/break_continue_pass.py
+++ Basics/loops/break_continue_pass.py
@@ -1,19 +1,18 @@
+x = 1
 for i in range(5):
-  if i==3:
+  if i == 3:
     break
   print("hello ",i)  
-
-for i in range(1,20):
+print("------------------------------")
+for i in range(1, 10):
   if i%3==0 or i%5==0:
     continue
   print(i)
-print("Bye")
-
+print("------------------------------")
 
 #to run a empty function we can use pass
-def fun():
-  pass
-
+if x == 1:
+  pass # this is used when we want to write code later but want to run the program without any error
 print("testing pass keyword")
 
 # python3 Basics/loops/break_continue_pass.py
