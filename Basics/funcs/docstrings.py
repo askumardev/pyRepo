b9563d00bda94afb040753d1ee4b48957c28c96a
askumardev <askumar.dev@outlook.com>
2026-03-22 20:02:10 +0530

    docstring help

--- Basics/funcs/docstrings.py
+++ Basics/funcs/docstrings.py
@@ -10,3 +10,4 @@ def sum(a, b): #a, b are parameters of the function sum
 print(sum.__doc__) # this will print the docstring of the function sum
 print(sum(5, 10)) #5, 10 are arguments
 # this will print the sum of 5 and 10
+help(sum) # this will also print the docstring of the function sum
