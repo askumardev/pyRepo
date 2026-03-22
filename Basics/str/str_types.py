6e52eccc1f2331e274dd64f132bba15506365a0e
askumardev <askumar.dev@outlook.com>
2026-03-22 07:53:58 +0530

    str types

---
+++ Basics/str/str_types.py
@@ -0,0 +1,23 @@
+str1 = "Hello World"
+print(str1)  # Hello World
+
+str2 = 'Python is great'
+print(str2)  # Python is great
+
+str3 = """This is a multi-line string.
+It can span multiple lines."""
+print(str3)
+
+str4 = '''Another multi-line string.
+It also can span multiple lines.'''
+print(str4)
+print("-----------------------------")
+a = 'Hello'
+b = "Hello"
+print(a == b)  # True
+
+# 🔥 Real-World Guideline
+# Use single quotes ' ' by default
+# Use double quotes " " when the string contains '
+# Use triple double quotes """ """ for docstrings and multi-line text
+# python3 Basics/str/str_types.py
