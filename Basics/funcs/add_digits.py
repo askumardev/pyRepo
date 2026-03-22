901c6ac56a505f11e77a8487a21e16abf1a024dc
askumardev <askumar.dev@outlook.com>
2026-03-22 19:51:46 +0530

    add_digits

--- Basics/funcs/add_digits.py
+++ Basics/funcs/add_digits.py
@@ -10,6 +10,14 @@ def add_digits(num):
     sum_digits = sum(arr)
     return add_digits(sum_digits)
 
+def sum_of_digits(num):
+    if isinstance(num, str):
+        num = int(num)
+    if num == 0:
+        return 0
+    
+    return (num % 10) + sum_of_digits(num // 10)
+
 print(add_digits(12))
 print(add_digits(123))
 print(add_digits(1234))
@@ -17,3 +25,6 @@ print(add_digits(8))
 
 print(add_digits("1234"))
 print(add_digits("12"))
+print("------------------")
+print(sum_of_digits(9876))
+print(sum_of_digits("9876"))
