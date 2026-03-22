4d40d3b7184799e35cac3eb9efe9a99d5d4ca326
askumardev <askumar.dev@outlook.com>
2026-03-22 18:38:50 +0530

    lambda

---
+++ Basics/funcs/lambda.py
@@ -0,0 +1,14 @@
+sq  = lambda x: x**2
+cube = lambda x: x**3
+sum = lambda a, b: a + b
+
+'''
+def sum(a, b):
+    return a + b
+'''
+
+print("Square of 5:", sq(5))   # Output: 25
+print("Cube of 3:", cube(3))   # Output: 27
+print("Sum of 4 and 6:", sum(4, 6))   # Output: 10
+
+# python3 Basics/funcs/lambda.py
