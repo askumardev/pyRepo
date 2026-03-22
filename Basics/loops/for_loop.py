b6bd15992e9f0cf1b50387348c728f51544b180a
askumardev <askumar.dev@outlook.com>
2026-03-22 06:55:06 +0530

    for loop update

--- Basics/loops/for_loop.py
+++ Basics/loops/for_loop.py
@@ -1,19 +1,25 @@
 x = [1,2,3]
 str = "satish"
-print(x)
+#print(x)
 
-for i in x:
+# for i in x:
+#   print(i)
+
+# for i in str:
+#   print(i)
+
+for i in range(5):
   print(i)
-
-for i in str:
+print("-----------------------------")
+for i in range(1,6):
   print(i)
-
-for i in range(10):
-  print(i)
-
+print("-----------------------------")
+for i in range(1,11):
+  print("5 X",i," = ", 5*i)
+print("-----------------------------")
 for i in range(11,21,2):
   print(i)
-
+print("-----------------------------")
 for i in range(20,10,-2):
   print(i)
 
