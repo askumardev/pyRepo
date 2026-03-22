4885c7cd7948f477f3090a934203553f76a0ca2a
askumardev <askumar.dev@outlook.com>
2026-03-22 06:31:32 +0530

    input even odd

---
+++ Basics/input.py
@@ -0,0 +1,4 @@
+# python3 Basics/input.py
+
+a = int(input("Enter value a :"))
+print("Value of a is:", a)
---
+++ Basics/loops/even_odd.py
@@ -0,0 +1,6 @@
+num  = int(input("Enter value :"))
+if num % 2 == 0:
+    print(num, "is an even number")
+else:    
+    print(num, "is an odd number")
+# python3 Basics/loops/even_odd.py
